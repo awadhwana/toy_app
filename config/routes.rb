2dd5b888db8dcd993bde3534271accf00d87708c
awadhwana <24ankitw@gmail.com>
2018-12-03 19:58:16 +0530

    layout and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   root 'home#home'
+  get '/signup', to: 'users#new'
   get  '/help', to: 'home#help'
   get  '/about', to: 'home#about'
   get  '/contact', to: 'home#contact'
