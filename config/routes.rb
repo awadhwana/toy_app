031ad83e0e665ab359e80ae0aaa5a512c7b2e955
awadhwana <24ankitw@gmail.com>
2018-12-14 18:26:01 +0530

    add password reset

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,19 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
+  get 'password_resets/new'
+  get 'password_resets/edit'
   get 'sessions/new'
   root 'home#home'
+
+  resources :password_resets, only: %i[new create edit update]
   resources :users, except: :new
   resources :account_activations, only: [:edit]
   get    '/signup', to: 'users#new'
   post   '/signup', to: 'users#create'
-  get      '/help', to: 'home#help'
-  get     '/about', to: 'home#about'
-  get   '/contact', to: 'home#contact'
+  get '/help', to: 'home#help'
+  get '/about', to: 'home#about'
+  get '/contact', to: 'home#contact'
   get     '/login', to: 'sessions#new'
   post    '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
